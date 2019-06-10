0b7aeb6367126430221f6ca4c0a325162de12e5f
deepanshu <deepanshu@tothenew.com>
2019-06-10 22:51:21 +0530

    complaint component

--- src/components/Complaint.js
+++ src/components/Complaint.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
 
 class Complaint extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            department:'admin',
+            concern:''
+        }
+    }
     render() {
         return (
             <div>
-                Complaint component
+                <form action="">
+                    <label htmlFor="department">Select Department</label>
+                    <select name={'department'} id="">
+                        <option value="Admin">Admin</option>
+                        <option value="IT">IT</option>
+                        <option value="HR">HR</option>
+                    </select>
+                    <label htmlFor="issue">Issue Title</label>
+                    <select name="" id="">
+                        <option value="software">Software</option>
+                        <option value="hardware">Hardware</option>
+                    </select>
+                    <label htmlFor="name">Your Name</label>
+                    <input type="text" />
+                    <label htmlFor="email">Email id</label>
+                    <input type="text"/>
+                    <label htmlFor="concern">Your Concern</label>
+                    <textarea name={'concern'}cols="30" rows="10" />
+                    <input type="submit"/>
+                </form>
             </div>
         );
     }
