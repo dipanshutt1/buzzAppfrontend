35a1830ca4a10dc85b320645de9c59de747b8175
deepanshu <deepanshu@tothenew.com>
2019-06-10 22:50:30 +0530

    form craeted

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,49 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            buzzContent:'',
+            category:'activity',
+            image:[]
+        }
+    }
+
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        if(this.state.buzzContent){
+            this.setState({
+                buzzContent:'',
+                category:'activity',
+                image:[]
+            })
+        }
+        else{
+            alert('write something in buzz');
+        }
+    };
+
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
     render() {
         return (
             <div>
-                Form component
+                <form onSubmit={this.handleSubmit}>
+                    <textarea name={'buzzContent'}
+                              value={this.state.buzzContent}
+                              placeholder={'Create your buzz here!!!!!'}
+                    />
+                    <select name={'category'} onChange={this.handleChange}>
+                        <option value={'activity'}>Activity</option>
+                        <option value={'LostFound'}>Lost & Found</option>
+                    </select>
+                    <input type={'submit'} value={'POST'}/>
+                </form>
             </div>
         );
     }
