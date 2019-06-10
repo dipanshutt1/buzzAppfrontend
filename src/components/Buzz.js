a95043ea2c72e14d58dda761c023e831bb3e962b
deepanshu <deepanshu@tothenew.com>
2019-06-10 22:52:09 +0530

    buzz got form

--- src/components/Buzz.js
+++ src/components/Buzz.js
@@ -1,44 +1,12 @@
 import React,{Component} from "react";
+import Form from "./Form";
 
 export default class BuzzComponent extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            buzzContent:'',
-            category:'activity',
-            image:[]
-        }
-    }
-
-    handleSubmit=(e)=>{
-        e.preventDefault();
-        if(this.state.buzzContent){
-            this.setState({
-                buzzContent:'',
-                category:'activity',
-                image:[]
-            })
-        }
-        else{
-            alert('write something in buzz');
-        }
-    };
-
 
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <textarea name={'buzzContent'}
-                              value={this.state.buzzContent}
-                              placeholder={'Create your buzz here!!!!!'}
-                    />
-                    <select name={'category'}>
-                        <option value={'activity'}>Activity</option>
-                        <option value={'LostFound'}>Lost & Found</option>
-                    </select>
-                    <input type={'submit'} value={'POST'}/>
-                </form>
+                <Form/>
             </div>
         )
     }
