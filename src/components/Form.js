2c5cf36822457ec2cb2eb46456160bfdbc20f265
deepanshu <deepanshu@tothenew.com>
2019-06-11 22:57:32 +0530

    basic form created for testing of api

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,47 +1,42 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            buzzContent:'',
-            category:'activity',
-            image:[]
-        }
-    }
-
-    handleSubmit=(e)=>{
-        e.preventDefault();
-        if(this.state.buzzContent){
-            this.setState({
-                buzzContent:'',
-                category:'activity',
-                image:[]
-            })
-        }
-        else{
-            alert('write something in buzz');
-        }
-    };
-
-    handleChange=(e)=>{
-        this.setState({
-            [e.target.name]:e.target.value
-        })
-    }
-
+    // constructor(props){
+    //     super(props);
+    //     this.state={
+    //         buzzContent:'',
+    //         category:'activity',
+    //         image:[]
+    //     }
+    // }
+    //
+    // handleChange=(e)=>{
+    //     this.setState({
+    //         [e.target.name]:e.target.value
+    //     })
+    // }
+    //
+    // handleSubmit=(e)=>{
+    //     this.setState({
+    //
+    //     })
+    // }
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <textarea name={'buzzContent'}
-                              value={this.state.buzzContent}
+                <form
+                      action='http://localhost:8080/dashboard/buzz'
+                      method="POST"
+                      encType={'multipart/form-data'}
+                      >
+                    <textarea name='buzzContent'
                               placeholder={'Create your buzz here!!!!!'}
                     />
                     <select name={'category'} onChange={this.handleChange}>
                         <option value={'activity'}>Activity</option>
                         <option value={'LostFound'}>Lost & Found</option>
                     </select>
+                    <input type="file" name='image'/>
                     <input type={'submit'} value={'POST'}/>
                 </form>
             </div>
