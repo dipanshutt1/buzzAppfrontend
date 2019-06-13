7829731faedbea4b0607c5b59d8ba848d69101b9
deepanshu <deepanshu@tothenew.com>
2019-06-14 00:49:14 +0530

    updated for reducer

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,47 +1,84 @@
 import React, {Component} from 'react';
+import {addBuzz} from '../action/buzzAction';
+import {connect} from 'react-redux';
 
 class Form extends Component {
-    // constructor(props){
-    //     super(props);
-    //     this.state={
-    //         buzzContent:'',
-    //         category:'activity',
-    //         image:[]
-    //     }
-    // }
-    //
-    // handleChange=(e)=>{
-    //     this.setState({
-    //         [e.target.name]:e.target.value
-    //     })
-    // }
-    //
-    // handleSubmit=(e)=>{
-    //     this.setState({
-    //
-    //     })
-    // }
+    constructor(props){
+        super(props);
+        this.state={
+            buzzContent:'',
+            category:'activity',
+            image:[]
+        }
+    }
+
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        const newBuzz=this.state;
+        const formData=new FormData();
+        formData.append('buzzContent',newBuzz.buzzContent);
+        formData.append('category',newBuzz.category);
+        formData.append('image',newBuzz.image);
+
+
+        this.props.addBuzz(formData)
+        this.setState({
+            buzzContent:'',
+            category:'activity',
+            image:[]
+        })
+    }
+    handleFileUpload=(e)=>{
+        this.setState({
+            image:e.target.files[0]
+        });
+    }
     render() {
         return (
             <div>
-                <form
+                <form onSubmit={this.handleSubmit}
                       action='http://localhost:8080/dashboard/buzz'
                       method="POST"
                       encType={'multipart/form-data'}
                       >
                     <textarea name='buzzContent'
                               placeholder={'Create your buzz here!!!!!'}
-                    />
+                              value={this.state.buzzContent}
+                              onChange={this.handleChange}                    />
                     <select name={'category'} onChange={this.handleChange}>
                         <option value={'activity'}>Activity</option>
                         <option value={'LostFound'}>Lost & Found</option>
                     </select>
-                    <input type="file" name='image'/>
-                    <input type={'submit'} value={'POST'}/>
+                    <input type="file"
+                           name='image'
+                           accept='image/*'
+                           value={this.state.image}
+                           onChange={this.handleFileUpload}
+                    />
+                    <input type={'submit'}
+                           value={'POST'}
+                    />
                 </form>
             </div>
         );
     }
 }
+const mapStateToProps = (state) => {
+    console.log("inside mapStateToProps", state);
+    return { userfeed: state.buzzReducer }
+}
 
-export default Form;
+// const mapDispatchToProps = (dispatch) => ({
+// addBuzz: (data) => dispatch(addBuzz(data))
+// });
+const mapDispatchToProps = {
+    addBuzz
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form)
