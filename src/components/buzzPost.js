a3e491f43ae42b28253d1bece784a999144d55a2
deepanshu <deepanshu@tothenew.com>
2019-06-14 18:00:07 +0530

    buzzpost added

--- src/components/buzzPost.js
+++ src/components/buzzPost.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+import BuzzThread from "./buzzThread";
+import {showBuzz} from "../action/buzzAction";
 
-class BuzzFeed extends Component {
+class BuzzPost extends Component {
+    componentDidMount() {
+        this.props.showBuzz();
+        console.log(this.props);
+    }
+
     render() {
+        console.log('heheke',this.props.data)
         return (
             <div>
-                
+                <ul>
+                    {this.props.userPost.map((data)=>{
+                        return(
+                            <BuzzThread post={data}/>
+                        )
+                    })}
+                </ul>
             </div>
         );
     }
 }
 
-export default BuzzFeed;
+const mapStateToProps=(state)=>{
+    console.log('mapStateToProps', state);
+    return{userPost:state.buzzReducer.buzzPost}
+}
+
+const mapDispatchToProps = {
+    showBuzz
+}
+export default connect(mapStateToProps,mapDispatchToProps)(BuzzPost);
