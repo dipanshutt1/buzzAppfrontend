8ddf1a17158433373cc3c37a8651439d748c8330
deepanshu <deepanshu@tothenew.com>
2019-06-14 17:58:25 +0530

    show post action added

--- src/action/buzzAction.js
+++ src/action/buzzAction.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axiosInstance from '../utils/axiosInterceptor';
 
 export const addBuzz=(formData)=> dispatch=> {
-    axios({
+    axiosInstance({
         method:'post',
         url:"http://localhost:8080/dashboard/buzz",
         data:formData,
@@ -13,14 +13,37 @@ export const addBuzz=(formData)=> dispatch=> {
     })
         .then(res=>{
             if(res.data.message='data storage done'){
+                console.log(`hehehe ${res.data.data}`);
                 dispatch(addBuzzPostToState(res.data.data));
             }
-        })
+        }).catch((err) => {
+            console.error(err);
+    })
 }
-
 export const addBuzzPostToState=(data)=>{
     return {
         type:'BUZZ_POST',
+        data
+    }
+}
+export const showBuzz=()=> dispatch => {
+    axiosInstance({
+        method:'get',
+        url:'http://localhost:8080/dashboard/buzz',
+        config: {
+            headers:{
+                'Content-Type':'multipart/form-data'
+            }
+        }
+    })
+        .then(res=>{
+            console.log("res.data",res.data)
+            dispatch(showBuzzPostToState(res.data))
+        })
+}
+export const showBuzzPostToState=(data)  =>{
+    return {
+        type:'SHOW_POST',
         data
     }
 }
---
+++ src/components/buzzPost.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class BuzzFeed extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default BuzzFeed;
