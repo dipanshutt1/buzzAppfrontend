68bd77da3cc6b1423015753d25f79c7a94cba239
deepanshu <deepanshu@tothenew.com>
2019-06-18 08:24:26 +0530

    store updated

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
-import{createStore,applyMiddleware} from "redux";
+import{createStore,applyMiddleware, compose} from "redux";
 import buzzReducer from "../reducer/buzzReducer";
+import complaintReducer from '../reducer/complaintReducer';
 import thunk from 'redux-thunk';
 import reducers from '../reducer/combineReducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store=createStore(
     reducers,
-    applyMiddleware(
-        thunk
-    )
+    composeEnhancers(applyMiddleware(thunk))
 );
 export default store;
