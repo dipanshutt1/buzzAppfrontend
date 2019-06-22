c643d3be834018974616bc1f598dc12aa88e11e6
deepanshu <deepanshu@tothenew.com>
2019-06-22 15:24:21 +0530

    resolve action updated

--- src/action/resolveAction.js
+++ src/action/resolveAction.js
@@ -12,13 +12,50 @@ export const showAllComplaint=()=> dispatch=>{
     })
         .then(res=>{
             console.log('res data is',res.data)
-            dispatch(showAllComplaintToState(res.data))
+            dispatch(showComplaintToState(res.data))
         })
-}
+};
 
-export const showAllComplaintToState=(data) =>{
+export const showComplaintToState=(data) =>{
     return{
-        type:'SHOW_ALL_COMPLAINT',
+        type:'SHOW_COMPLAINT',
         data
     }
+};
+
+export const showMyComplaint=()=> dispatch=>{
+    console.log('hello');
+    axiosInstance({
+        method:'get',
+        url:'http://localhost:8080/dashboard/resolve/myComplaint',
+        config:{
+            headers:{
+                'Content-Type':'multipart/form-data'
+            }
+        }
+    })
+        .then(res=>{
+            console.log('res data is',res.data);
+            dispatch(showComplaintToState(res.data))
+        })
+};
+
+export const resolveCommit=(formData)=>dispatch=>{
+    axiosInstance({
+        method:'post',
+        url:"http://localhost:8080/dashboard/resolve/myComplaint",
+        data:formData
+            .then(res=>{
+                dispatch(addCommitToState(res.data))
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+    })
 }
+
+const addCommitToState=(data)=>
+    ({
+        type:'RESOLVE_COMMIT',
+        data
+});
