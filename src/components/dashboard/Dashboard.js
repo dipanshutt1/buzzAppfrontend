2eeaa7f299c25257cff0543c52d928857812aa1f
deepanshu <deepanshu@tothenew.com>
2019-06-24 22:56:46 +0530

    dashboard updated for all user case

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import Menu from './Menu';
-import Complaint from './complaint/complaint'
-import Resolve from './resolve/Resolve';
+import Menu from '../menu/Menu';
+import Complaint from '../complaint/complaint'
+import Resolve from '../resolve/Resolve';
 import {Route} from 'react-router-dom';
-import Buzz from './buzz/Buzz';
-import User from './userProfile/user'
+import Buzz from '../buzz/Buzz';
+import User from '../userProfile/user'
 import './dashboard.css'
 import {connect} from "react-redux";
+import banner from '../../assets/banner.jpeg'
+import AllUser from "../allUser/allUser";
+import ttnlogo from '../../assets/ttnlogo.png';
 
 class Dashboard extends Component {
 
@@ -20,9 +23,22 @@ class Dashboard extends Component {
             <div>
                 <header>
                     <nav className='navbarLogout'>
-                        <button onClick={this.logout}>Logout</button>
+                        <img src={ttnlogo} alt="image/*"/>
+                        <i onClick={this.logout} className="fa fa-sign-out fa-3x" aria-hidden="true"></i>
                     </nav>
-                    <div className='banner'></div>
+                    <div id="carouselExampleSlidesOnly" className="carouselBanner carousel slide" data-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img className="bannerItem d-block w-100" src={banner} accept='image/*'></img>
+                            </div>
+                            <div className="carousel-item">
+                                <img className="bannerItem d-block w-100" src={banner} accept='image/*'></img>
+                            </div>
+                            <div className="carousel-item">
+                                <img className="bannerItem d-block w-100" src={banner} accept='image/*'></img>
+                            </div>
+                        </div>
+                    </div>
                 </header>
                 <main>
                     <aside>
@@ -43,8 +59,11 @@ class Dashboard extends Component {
                             exact path="/dashboard/resolve"
                             component={Resolve}
                         />}
-
-
+                        {this.props.user.userProfile.userRole==='USER' ? null :
+                            <Route
+                                exact path="/dashboard/allUser"
+                                component={AllUser}
+                            />}
                     </section>
                 </main>
             </div>
