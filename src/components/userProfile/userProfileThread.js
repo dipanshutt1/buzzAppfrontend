dac926f8c10c833edb22014f142b060d13fe8997
deepanshu <deepanshu@tothenew.com>
2019-06-24 23:05:46 +0530

    user details are shown

--- src/components/userProfile/userProfileThread.js
+++ src/components/userProfile/userProfileThread.js
@@ -3,14 +3,17 @@ import React, {Component} from 'react';
 class UserProfileThread extends Component {
     render() {
         console.log(this.props.user);
-        const{userName, userImg}=this.props.user
+        const{userName, userImg,email,department,userRole}=this.props.user
         return (
-            <div>
-                <ul>
-                    <img src={userImg}/>
-                    <li>{userName}</li>
-                </ul>
-            </div>
+            <React.Fragment>
+                    <img className="img-thumbnail img-fluid rounded-circle" src={userImg}/>
+                    <div className="profileWrapper">
+                        <h4 className="text-center">{userName}</h4>
+                        <h6 className="text-center">{department}</h6>
+                        {/*<h6 className="text-center">Role : {userRole}</h6>*/}
+                        <h6 className="text-center">{email}</h6>
+                    </div>
+            </React.Fragment>
         );
     }
 }
