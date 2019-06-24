46b89d3859737e083c64e5529b93257aa89fdbdb
deepanshu <deepanshu@tothenew.com>
2019-06-24 22:57:31 +0530

    menu updated for all user component

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react'
 import {Link } from 'react-router-dom';
 import {connect} from "react-redux";
-
+import './menu.css';
 
 class Menu extends Component {
     render() {
+        console.log('Menu Render',this.props.user.userProfile);
         return (
-            <div>
-
-                    <button className="btn btn-outline-primary btn-lg btn-block">
+            <div className="menu">
+                    <button className="myBtn btn-outline-primary btn btn-block">
                         <Link to="/dashboard/buzz">
                             Buzz
                         </Link>
                     </button>
-                    <button className="btn btn-outline-primary btn-lg btn-block">
+                    <button className="myBtn btn btn-outline-primary btn-block">
                         <Link to="/dashboard/complaint">
                             Complaint
                         </Link>
                     </button>
-                    {this.props.user.userProfile.userRole === 'USER' ? null : <button className="btn btn-outline-primary btn-lg btn-block">
+                    {this.props.user.userProfile.userRole === 'USER' ? null : <button className="myBtn btn btn-outline-primary btn-block">
                         <Link to="/dashboard/resolve">
                             Resolve
                         </Link>
                     </button>}
-
+                    {this.props.user.userProfile.userRole==='USER' ? null :
+                    <button className="myBtn btn btn-outline-primary btn-block">
+                        <Link to="/dashboard/allUser">
+                            All Users
+                        </Link>
+                    </button>}
 
 
             </div>
