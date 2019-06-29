4971c10ef7b96c7464ca4ae073fbf79a026e5130
dipanshutt1 <deepanshu@tothenew.com>
2019-06-29 14:15:55 +0530

    updated

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom';
 import {connect} from "react-redux";
 import './menu.css';
+import mapDispatchToProps from "react-redux/es/connect/mapDispatchToProps";
 
 class Menu extends Component {
     render() {
@@ -10,19 +11,23 @@ class Menu extends Component {
             <div className="menu">
 
                 <Link className=" myBtn buzz" to="/dashboard/buzz">
-                    Buzz
+                    <i className="fa fa-rss" aria-hidden="true"></i>
+                    <span>Buzz</span>
                 </Link>
                 <Link className="myBtn buzz" to="/dashboard/complaint">
-                    Complaint
+                    <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    <span>Complaint</span>
                 </Link>
                 {this.props.user.userProfile.userRole === 'USER' ? null :
                     <Link className="myBtn buzz" to="/dashboard/resolve">
-                        Resolve
+                        <i className="fa fa-check" aria-hidden="true"></i>
+                        <span>Resolve</span>
                     </Link>
                 }
                 {this.props.user.userProfile.userRole === 'IT' ?
                     <Link className="myBtn buzz" to="/dashboard/allUser">
-                        All Users
+                        <i className="fa fa-users" aria-hidden="true"></i>
+                        <span>All Users</span>
                     </Link>: null
 
                 }
@@ -38,5 +43,5 @@ const mapStateToProps = (store) => {
     }
 };
 
-export default connect(mapStateToProps, undefined)(Menu);
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
 
