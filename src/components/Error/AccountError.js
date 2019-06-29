e39b2f2e5d91e13ed6587380a44ab1c0e2d281aa
dipanshutt1 <deepanshu@tothenew.com>
2019-06-29 14:15:35 +0530

    deactivate user error component added

---
+++ src/components/Error/AccountError.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AccountError extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Your Account has been deactivated</h2>
+            </div>
+        );
+    }
+}
+
+export default AccountError;
