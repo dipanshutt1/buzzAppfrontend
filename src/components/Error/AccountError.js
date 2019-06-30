ad9b0284938b81c591f620185dcf3873e14b500f
dipanshutt1 <deepanshu@tothenew.com>
2019-06-30 14:06:52 +0530

    error handler page done for deactivated accounts

--- src/components/Error/AccountError.js
+++ src/components/Error/AccountError.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-
+import './error.css';
 class AccountError extends Component {
     render() {
         return (
-            <div>
-                <h2>Your Account has been deactivated</h2>
+            <div className="error-component">
+                <h1>Your Account has been deactivated!!!</h1>
+                <h3>Kindly contact to the admin department</h3>
             </div>
         );
     }
