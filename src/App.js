8b78c6063194dd1116caa1d56cc4bc107a59ca69
dipanshutt1 <deepanshu@tothenew.com>
2019-07-01 10:34:44 +0530

    Wrong routes handled

--- src/App.js
+++ src/App.js
@@ -1,45 +1,64 @@
 import React,{Component} from 'react';
 import './App.css';
-import Login from './components/login/login';
-import {Redirect, Route, BrowserRouter as Router} from "react-router-dom";
-import Dashboard from "./components/dashboard/Dashboard";
-import TokenComponent from './components/Token'
-import User from "./components/userProfile/user";
-import AccountError from './components/Error/AccountError';
+import Routes from './routes';
+import {BrowserRouter} from "react-router-dom";
 
-export default class App extends Component {
-  render() {
-     return (
-        <div>
-            <Router>
-                <Route exact path={'/'} component={Login}/>
-                <Route path={'/token'} component={TokenComponent}/>
-                <Route path={'/accountError'} component={AccountError}/>
-                <PrivateRoute path={'/dashboard'} component={Dashboard}/>
-            </Router>
-        </div>
-    );
-  }
-}           
-
-const PrivateRoute = ({component: Component, ...rest}) => {
-    if(localStorage.getItem('token')) {
-        return <Route {...rest} render={(props) => {
-            return (
-                <React.Fragment>
-                {/*<User />*/}
-                <Component {...props}/>
-                </React.Fragment>
+export default class App extends Component{
+    render() {
+        return(
+            <div>
+                <BrowserRouter>
+                    <Routes />
+                </BrowserRouter>
+            </div>
         )
-        }
-        }/>
-
-    } else {
-        return <Route {...rest} render={(props) => {
-            return <Redirect {...props} to={'/'}/>
-        }
-        }/>
     }
-};
+}
 
 
+
+// import Login from './components/login/login';
+// import {Redirect, Route, BrowserRouter as Router} from "react-router-dom";
+// import Dashboard from "./components/dashboard/Dashboard";
+// import TokenComponent from './components/Token'
+// import AccountError from './components/Error/AccountError';
+// import ErrorPage from './components/Error/ErrorPage'
+//
+// export default class App extends Component {
+//   render() {
+//      return (
+//         <div>
+//             <Router>
+//                 <Route exact path={'/'} component={Login}/>
+//                 <Route exact path={'/token'} component={TokenComponent}/>
+//                 <Route exact path={'/accountError'} component={AccountError}/>
+//                 <PrivateRoute exact path={'/dashboard'} component={Dashboard}/>
+//                 <Route exact path='/pagenotfound' component={ErrorPage}/>
+//                 {/*<Redirect from='*' to='/pagenotfound' />*/}
+//             </Router>
+//         </div>
+//     );
+//   }
+// }
+//
+// const PrivateRoute = ({component: Component, ...rest}) => {
+//     if(localStorage.getItem('token')) {
+//         return <Route {...rest} render={(props) => {
+//             return (
+//                 <React.Fragment>
+//                 {/*<User />*/}
+//                 <Component {...props}/>
+//                 </React.Fragment>
+//         )
+//         }
+//         }/>
+//
+//     } else {
+//         return <Route {...rest} render={(props) => {
+//             return <Redirect {...props} to={'/'}/>
+//         }
+//         }/>
+//     }
+// };
+//
+//
--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,27 @@ import Dashboard from "./components/dashboard/Dashboard";
 import TokenComponent from './components/Token'
 import AccountError from './components/Error/AccountError';
 import ErrorPage from './components/Error/ErrorPage'
+import Resolve from "./components/resolve/Resolve";
+import Buzz from "./components/buzz/Buzz";
+import Complaint from './components/complaint/complaint'
 
-export default class Router extends Component {
+export default class Routes extends Component {
     render() {
         return (
             <div>
                 <Switch>
+                    <Route exact path='/pagenotfound' component={ErrorPage}/>
                     <Route exact path={'/'} component={Login}/>
                     <Route exact path={'/token'} component={TokenComponent}/>
                     <Route exact path={'/accountError'} component={AccountError}/>
-                    <PrivateRoute exact path={'/dashboard'} component={Dashboard}/>
-                    <Route exact path='/pagenotfound' component={ErrorPage}/>
-                    <Redirect from='*' to='/pagenotfound' />
+                    <PrivateRoute path={'/dashboard'} component={Dashboard}/>
+                    <Redirect
+                        to={
+                            {
+                                pathname: '/pagenotfound'
+                            }
+                        }
+                    />
                 </Switch>
             </div>
         );
