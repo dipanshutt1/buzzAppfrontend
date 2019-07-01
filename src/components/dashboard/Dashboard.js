2ab665cc6404a5a037819816f3d7549786d1703f
dipanshutt1 <deepanshu@tothenew.com>
2019-07-01 21:25:04 +0530

    routing and component rendering done properly

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Menu from '../menu/Menu';
 import Complaint from '../complaint/complaint'
 import Resolve from '../resolve/Resolve';
-import {Route} from 'react-router-dom';
+import {Redirect, Route, Switch} from 'react-router-dom';
 import Buzz from '../buzz/Buzz';
 import User from '../userProfile/user'
 import './dashboard.css'
@@ -10,6 +10,7 @@ import {connect} from "react-redux";
 import AllUser from "../allUser/allUser";
 import ttnlogo from '../../assets/ttnlogo.png';
 import banner2 from '../../assets/banner2.jpg';
+import ErrorPage from "../Error/ErrorPage";
 
 class Dashboard extends Component {
 
@@ -24,35 +25,66 @@ class Dashboard extends Component {
                 <header>
                     <nav className='navbarLogout '>
                         <a href="https://www.tothenew.com"><img src={ttnlogo} alt="image/*"/></a>
-                        <i onClick={this.logout} style ={{color:'#091341'}}className="fa fa-sign-out fa-2x" aria-hidden="true"></i>
+                        <i onClick={this.logout} style={{color: '#091341'}} className="fa fa-sign-out fa-2x"
+                           aria-hidden="true"></i>
                     </nav>
-                        <div>
-                            <img className="bannerItem d-block w-100" src={banner2} accept='image/*'></img>
-                        </div>
-            </header>
+                    <div>
+                        <img className="bannerItem d-block w-100" src={banner2} accept='image/*'></img>
+                    </div>
+                </header>
                 <main>
                     <aside className={"col-lg-3"}>
                         <User/>
                         <Menu/>
                     </aside>
                     <section>
-                        <Route
-                            exact path={`${this.props.match.path}/buzz`}
-                            component={Buzz}
-                        />
-                        <Route
-                            exact path="/dashboard/complaint"
-                            component={Complaint}
-                        />
-                        {this.props.user.userProfile.userRole === 'USER' ? null : <Route
-                            exact path="/dashboard/resolve"
-                            component={Resolve}
-                        />}
-                        {this.props.user.userProfile.userRole==='IT' ? <Route
-                            exact path="/dashboard/allUser"
-                            component={AllUser}
-                        /> : null
+                        <Switch>
+
+                            <Route
+                                exact
+                                path={`${this.props.match.path}/`}
+                                render={() => <Redirect to={
+                                    {
+                                        pathname: '/dashboard/buzz'
+                                    }
+                                }/>}
+                            />
+
+                            <Route
+                                exact
+                                strict
+                                path={`${this.props.match.path}/buzz`}
+                                component={Buzz}
+                            />
+                            <Route
+                                exact path="/dashboard/complaint"
+                                component={Complaint}
+                            />
+                            {
+                                this.props.user.userProfile.userRole === 'USER' ?
+                                    null :
+                                    <Route
+                                        exact path="/dashboard/resolve"
+                                        component={Resolve}
+                                    />
                             }
+                            {
+                                this.props.user.userProfile.userRole === 'IT' ?
+                                    <Route
+                                        exact path="/dashboard/allUser"
+                                        component={AllUser}
+                                    /> :
+                                    null
+                            }
+                            <Route to={"*"} component={Buzz} />
+                            {/*<Redirect*/}
+                            {/*    to={*/}
+                            {/*        {*/}
+                            {/*            pathname: '/pagenotfound'*/}
+                            {/*        }*/}
+                            {/*    }*/}
+                            />
+                        </Switch>
                     </section>
                 </main>
             </div>
