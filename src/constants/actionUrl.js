542e6a313f03aee56029ee6363274552c62a4d80
dipanshutt1 <deepanshu@tothenew.com>
2019-07-01 21:26:24 +0530

    constants are renamed

--- src/constants/actionUrl.js
+++ src/constants/actionUrl.js
@@ -3,18 +3,10 @@ export const BASE_URL = 'http://localhost:8080';
 export const DASHBOARD_URL=`${BASE_URL}/dashboard`;
 export const BUZZ_URL=`${DASHBOARD_URL}/buzz`;
 export const LIKE_URL=`${BUZZ_URL}/like`;
-export const DISLIKE_URL=`${BASE_URL}/dislike`;
+export const DISLIKE_URL=`${BUZZ_URL}/dislike`;
 export const ALL_USER_URL=`${DASHBOARD_URL}/allUser`;
 export const ACCOUNT_STATUS_URL=`${ALL_USER_URL}/accountStatus`;
 export const COMPLAINT_URL=`${DASHBOARD_URL}/complaint`;
 export const RESOLVE_URL=`${DASHBOARD_URL}/resolve`;
 export const RESOLVE_MY_COMPLAINT_URL=`${RESOLVE_URL}/myComplaint`;
 export const USER_PROFILE_URL=`${DASHBOARD_URL}/userProfile`;
-
-
-
-// export const DASHBOARD_BUZZ_URL = `${BASE_URL}/dashboard/buzz`;
-// export const DASHBOARD_COMPLAINT_URL = `${BASE_URL}/dashboard/complaint`;
-// export const DASHBOARD_RESOLVE_URL = `${BASE_URL}/dashboard/resolve`;
-// export const USER_PROFILE_URL = `${BASE_URL}/userProfile`;
-// export const ALL_USER_URL=`${BASE_URL}/`
